b8daeaa915e327591b1e4402d4dbfffe77a08862
Huy Le <huyle333@yahoo.com>
2014-10-04 18:38:56 -0400

    current user integration conditionals and license/application key

--- src/com/example/hackmit/LoginPage.java
+++ src/com/example/hackmit/LoginPage.java
@@ -2,10 +2,13 @@ package com.example.hackmit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
 
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
+import com.parse.ParseAnonymousUtils;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 public class LoginPage extends Activity{
 	@Override
@@ -14,9 +17,22 @@ public class LoginPage extends Activity{
 		setContentView(R.layout.login_page);
 		
 		Parse.initialize(this, "ZF3tzb2RBU4J5PYLlVxgnlMGHa3SWkRAoI6Y2OAN", "34XmgZk4QgkOPqdStKzn3tNHQyNWoW5AeeEighYP");
-		ParseObject testObject = new ParseObject("TestObject");
-		testObject.put("foo", "bar");
-		testObject.saveInBackground();
+		
+		//Is current user an anonymous user or not?
+		if (ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())){
+			
+		}else{
+			// If current user is not an anonymous user
+			ParseUser currentUser = ParseUser.getCurrentUser();
+			if (currentUser != null){
+				// Send the logged in user to the MainActivity.class
+				Intent intent = new Intent(LoginPage.this, MainActivity.class);
+				startActivity(intent);
+				finish();
+			}else{
+				
+			}
+		}
 	}
 
 }
---
+++ src/com/example/hackmit/ParseApplication.java
@@ -0,0 +1,7 @@
+package com.example.hackmit;
+
+
+
+public class ParseApplication {
+
+}
