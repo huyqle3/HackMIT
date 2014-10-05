083c1d2743900f9cdffaf40a1e062e0a87fc5902
Huy Le <huyle333@yahoo.com>
2014-10-04 20:16:53 -0400

    adjusted strings.xml file along with reorganizing xml files

--- gen/com/example/hackmit/R.java
+++ gen/com/example/hackmit/R.java
@@ -24,11 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080009;
         public static final int button1=0x7f080004;
+        public static final int login=0x7f080007;
         public static final int pager=0x7f080000;
         public static final int password=0x7f080003;
         public static final int section_label=0x7f080001;
+        public static final int signup=0x7f080008;
+        public static final int txtpassword=0x7f080006;
+        public static final int txtusername=0x7f080005;
         public static final int username=0x7f080002;
     }
     public static final class layout {
@@ -41,6 +45,11 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int Login=0x7f050007;
+        public static final int Logout=0x7f050009;
+        public static final int Password=0x7f050006;
+        public static final int Signup=0x7f050008;
+        public static final int Username=0x7f050005;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int title_section1=0x7f050002;
